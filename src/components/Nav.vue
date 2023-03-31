068cefdff8e117d044ef26e30cf6d84b72ece933
Jet <1298874688@qq.com>
2023-03-31 15:26:52 +0800

    新增 nav 字体部分

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,37 @@
 <template>
     <nav>
+        <span>美瞳选购平台</span>
         <h3>Movo</h3>
-        <h4>美瞳选购平台</h4>
     </nav>
 </template>
-<style scoped></style>
+<style scoped>
+nav {
+    background-color: var(--color-orange-300);
+    position: relative;
+    height: 3.8rem;
+}
+
+/* logo */
+nav>h3 {
+    bottom: 0;
+    color: var(--color-white);
+    font-size: var(--text-3xl);
+    position: absolute;
+    vertical-align: bottom;
+    height: 4.5rem;
+    font-weight: 400;
+}
+
+/* 首字母放大 */
+nav>h3::first-letter {
+    font-size: var(--text-6xl);
+}
+
+nav>span {
+    position: absolute;
+    left: 4.2rem;
+    top: 0.5rem;
+    color: var(--color-white);
+    font-size: var(--text-xl);
+}
+</style>
