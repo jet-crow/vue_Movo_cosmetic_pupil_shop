ade8b135993116a5bf8c734d9f3eed52fa966d29
Jet <1298874688@qq.com>
2023-04-03 23:19:21 +0800

    insert Commodity商品展示插件，update 登录的nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
     <nav>
         <span>美瞳选购平台</span>
-        <h3>Movo</h3>
+        <h3>movo</h3>
     </nav>
 </template>
 <style scoped>
@@ -9,17 +9,19 @@ nav {
     background-color: var(--color-orange-300);
     position: relative;
     height: 3.8rem;
+    border-radius: 0 0 .8rem .8rem;
 }
 
 /* logo */
 nav>h3 {
     bottom: 0;
     color: var(--color-white);
-    font-size: var(--text-3xl);
+    font-size: var(--text-2xl);
     position: absolute;
     vertical-align: bottom;
-    height: 4.5rem;
     font-weight: 400;
+    line-height: 2.8rem;
+    padding-left: .8rem;
 }
 
 /* 首字母放大 */
@@ -29,7 +31,7 @@ nav>h3::first-letter {
 
 nav>span {
     position: absolute;
-    left: 4.2rem;
+    left: 3.5rem;
     top: 0.5rem;
     color: var(--color-white);
     font-size: var(--text-xl);
--- src/views/after/Login.vue
+++ src/views/after/Login.vue
@@ -1,8 +1,49 @@
 <template>
-    <Nav />
+    <!-- <Nav /> -->
+    <main>
+        <!-- <label>
+            <input v-model="user" />
+            <span>账号输入有误</span>
+        </label>
+        <label>
+            <input v-model="password" />
+            <span>密码输入有误</span>
+        </label> -->
+        <div style="border:1px solid; width: inherit; height: inherit;">
+            <van-swipe :autoplay="3000" :show-indicators="false">
+                <van-swipe-item style=" background-color: #39a9ed;">
+                    <div class="box" style="background-color: red;;">
+
+                    </div>
+                </van-swipe-item>
+                <van-swipe-item>
+                    <div style="background-color: yellow; height: 7rem;width: 20rem;"></div>
+                </van-swipe-item>
+                <van-swipe-item>
+                    <div style="background-color: blue; height: 7rem;width: 20rem;"></div>
+                </van-swipe-item>
+                <van-swipe-item>
+                    <div style="background-color: black; height: 7rem;width: 20rem;"></div>
+                </van-swipe-item>
+            </van-swipe>
+        </div>
+    </main>
 </template>
 <script setup>
 import Nav from '../../components/Nav.vue'
+import { ref } from 'vue'
+const user = ref()
+const password = ref()
 
 </script>
-<style scoped></style>
+<style scoped>
+main {
+    /* margin-top: 5rem; */
+    width: 100vw;
+    height: 34.5rem;
+    background-image: url(../../assets/image/日抛1/日抛1-顶图.png);
+    background-size: 100%;
+    display: flex;
+    flex-direction: column;
+}
+</style>
