fda5acd3a025dd25deff38de498ede38620124b1
Jet <1298874688@qq.com>
2023-04-04 13:58:58 +0800

    insert Commodity商品展示滑块，update Login 的Nav

---
+++ src/components/CommodityWindows.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="box">
+        <van-swipe :autoplay="3000" :show-indicators="false" class="outer_layer"
+            :style="` background-image: url(${getImgUrl(commodityData.bottomImg)});`">
+            <van-swipe-item v-for="(item, index) in commodityData.itemImg">
+                <img class="item" :src="getImgUrl(item)" />
+            </van-swipe-item>
+        </van-swipe>
+        <div class="outer_layer_after" :style="` background-image: url(${getImgUrl(commodityData.topImg)});`"></div>
+    </div>
+</template>
+
+<script setup>
+/* 
+传参
+const commodityData = {
+    topImg: "日抛1/日抛1_0000_顶图.png",
+    bottomImg: "日抛1/日抛1_0005_底图.png",
+    itemImg: ["日抛1/日抛1_0001_伽罗棕.png",
+        "日抛1/日抛1_0002_胶片棕.png", "日抛1/日抛1_0003_丝绒棕.png", "日抛1/日抛1_0004_柔咖棕.png"]
+}
+ */
+const props = defineProps(["commodityData"])
+
+function getImgUrl(src) {
+    return require("@/assets/images/" + src)
+}
+</script>
+<style scoped>
+.box {
+    border: 1px solid var(--color-white);
+    border-top: none;
+    width: 100vw;
+    position: relative;
+    border-bottom-left-radius: 1.8rem;
+    border-bottom-right-radius: 1.8rem;
+    overflow: hidden;
+}
+
+.outer_layer {
+    width: inherit;
+    /* background-image: url(@/assets/images/日抛1/日抛1_0005_底图.png); */
+    background-repeat: no-repeat;
+    background-size: 100%;
+}
+
+
+.outer_layer .item {
+    width: inherit;
+    /* 滤镜 */
+    /* mix-blend-mode: multiply; */
+}
+
+
+.outer_layer_after {
+    position: absolute;
+    z-index: 99;
+    top: 0;
+    width: 100vw;
+    height: 100%;
+    /* background-image: url(@/assets/images/日抛1/日抛1_0000_顶图.png); */
+    background-size: 100%;
+    background-repeat: no-repeat;
+    pointer-events: none;
+    border-radius: 20px;
+
+}
+</style>
--- src/views/after/Login.vue
+++ src/views/after/Login.vue
@@ -1,49 +1,47 @@
 <template>
-    <!-- <Nav /> -->
-    <main>
-        <!-- <label>
-            <input v-model="user" />
-            <span>账号输入有误</span>
-        </label>
-        <label>
-            <input v-model="password" />
-            <span>密码输入有误</span>
-        </label> -->
-        <div style="border:1px solid; width: inherit; height: inherit;">
-            <van-swipe :autoplay="3000" :show-indicators="false">
-                <van-swipe-item style=" background-color: #39a9ed;">
-                    <div class="box" style="background-color: red;;">
-
-                    </div>
-                </van-swipe-item>
-                <van-swipe-item>
-                    <div style="background-color: yellow; height: 7rem;width: 20rem;"></div>
-                </van-swipe-item>
-                <van-swipe-item>
-                    <div style="background-color: blue; height: 7rem;width: 20rem;"></div>
-                </van-swipe-item>
-                <van-swipe-item>
-                    <div style="background-color: black; height: 7rem;width: 20rem;"></div>
-                </van-swipe-item>
-            </van-swipe>
+    <Nav />
+    <main style="width: 100vw;height: 100vh; background-color: var(--color-white);">
+        <div class="login_box">
+            <!-- <label>
+                <input v-model="user" />
+                <span>账号输入有误</span>
+            </label>
+            <label>
+                <input v-model="password" />
+                <span>密码输入有误</span>
+            </label> -->
         </div>
     </main>
 </template>
 <script setup>
-import Nav from '../../components/Nav.vue'
+import Nav from '@/components/Nav.vue'
 import { ref } from 'vue'
+
+//账号和密码
 const user = ref()
 const password = ref()
 
+
 </script>
 <style scoped>
-main {
-    /* margin-top: 5rem; */
-    width: 100vw;
-    height: 34.5rem;
-    background-image: url(../../assets/image/日抛1/日抛1-顶图.png);
-    background-size: 100%;
+.login_box {
+    width: 23rem;
+    height: 35.8rem;
     display: flex;
     flex-direction: column;
+    background-color: var(--color-orange-100-85);
+    border-bottom-right-radius: 2rem;
+    position: relative;
+}
+
+.login_box::before {
+    content: "";
+    z-index: 10;
+    position: absolute;
+    width: .7rem;
+    height: 1rem;
+    left: 0;
+    top: -1rem;
+    background-color: var(--color-orange-300);
 }
 </style>
