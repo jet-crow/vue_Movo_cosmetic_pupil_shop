7389dc3aebfbf8978a401e0b690a8e2c3b769158
Drunkcat7 <Miaopasc@gmail.com>
2023-04-05 01:04:56 +0800

    login页

--- src/views/after/Login.vue
+++ src/views/after/Login.vue
@@ -1,30 +1,38 @@
 <template>
-    <Nav />
     <main>
+        <Nav />
+        <div class="banner">
+            <img src="../../assets/images/loginBg.jpg" alt="" srcset="">
+        </div>
         <div class="login_box">
-            <label class="login_item no_border">
-                <input v-model="user" /><br />
+            <label class="login_item_input no_border">
+                <input v-model="user" placeholder="usernames"/>
+                <!-- <span>&nbsp</span> -->
                 <span>账号输入有误</span>
             </label>
-            <label class="login_item no_border">
-                <input v-model="password" /><br />
+            <label class="login_item_input no_border">
+                <input v-model="password" placeholder="password" />
+                <!-- <span>&nbsp</span> -->
                 <span>密码输入有误</span>
             </label>
-            <label class="login_item">
-                <button class="login_btn">登录</button>
-            </label>
-            <label class="login_item">
-                <button class="login_btn">注册</button>
-            </label>
+
+            <div class="login_box_button">
+                <label class="login_item_button">
+                    <button class="login_btn">注册</button>
+                </label>
+                <label class="login_item_button">
+                    <button class="login_btn">登录</button>
+                </label>
+            </div>
         </div>
     </main>
 </template>
 <script setup>
-import Nav from '@/components/Nav.vue'
-import { ref } from 'vue'
+import Nav from '@/components/Nav.vue';
+import { ref } from 'vue';
 
 //账号和密码
-const user = ref()
+const user = ref();
 const password = ref()
 
 
