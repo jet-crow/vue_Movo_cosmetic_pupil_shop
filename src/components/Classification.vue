e7d348abb63a38b9600a8fcf6efebf0d0e855712
Jet <1298874688@qq.com>
2023-04-06 01:12:34 +0800

    update Index页面

---
+++ src/components/Classification.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="menu">
+        <div class="menu_item">
+            <img src="../../public/images/menu/5.png" />
+            <p>全部</p>
+        </div>
+        <div class="menu_item">
+            <img src="../../public/images/menu/1.png" />
+            <p>日抛</p>
+        </div>
+        <div class="menu_item">
+            <img src="../../public/images/menu/2.png" />
+            <p>月抛</p>
+        </div>
+        <div class="menu_item">
+            <img src="../../public/images/menu/3.png" />
+            <p>季抛</p>
+        </div>
+        <div class="menu_item">
+            <img src="../../public/images/menu/4.png" />
+            <p>半年抛</p>
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+.menu {
+    display: flex;
+    align-items: center;
+    text-align: center;
+    justify-content: space-around;
+}
+.menu_item{
+    margin: 0 .3rem;
+}
+.menu_item img {
+    width: 100%;
+}
+/* menu_item_selected 选中样式*/
+.menu_item_selected img {
+    filter: hue-rotate(320deg)
+}
+.menu_item_selected p{
+    color: var(--color-orange-400);
+}
+</style>
--- src/views/after/Index.vue
+++ src/views/after/Index.vue
@@ -2,21 +2,26 @@
     <Nav />
     <aside>
         <div class="banner">
-            <img src="../../../public/images/loginBg.jpg" alt="" srcset="">
+            <video webkit-playsinline="true" poster="../../../public/images/introduce.jpg"
+                src="../../../public/video/introduce.mp4" autoplay muted></video>
         </div>
+        <!-- 分类 -->
+        <Classification/>
     </aside>
 </template>
 
 <script setup>
 import Nav from '@/components/Nav.vue';
+import Classification from '@/components/Classification.vue'
 </script>
-
+<!-- introduce -->
 <style scoped>
-.banner img {
+.banner video {
     width: 100%;
     height: 100%;
-    object-fit: cover;
+    object-fit: fill;
     margin-top: .3rem;
-    border-radius: .8rem .8rem 0 0;
+    margin-bottom: .8rem;
+    border-radius: .8rem;
 }
 </style>
