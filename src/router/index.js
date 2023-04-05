4e8ceb39176a203c7e1e31ef26a5b95032ed17a2
Jet <1298874688@qq.com>
2023-04-05 15:34:12 +0800

    update 登录注册页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Login from "../views/after/Login.vue"
+import LoginAndRegistered from "../views/after/LoginAndRegistered.vue"
 
 
 //路由配置
 // {
 //   path: '/about',
 //   name: 'about',
-//   // route level code-splitting
-//   // this generates a separate chunk (about.[hash].js) for this route
-//   // which is lazy-loaded when the route is visited.
 //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
 // }
 const routes = [
   {
     path: '/',
-    name: 'login',
-    component: Login
+    name: 'loginAndRegistered',
+    component: LoginAndRegistered
   },
 ]
 
---
+++ src/views/after/LoginAndRegistered.vue
@@ -0,0 +1,59 @@
+<template>
+    <main>
+        <Nav />
+        <div class="banner">
+            <img src="../../assets/images/loginBg.jpg" alt="" srcset="">
+        </div>
+        <div class="form_box">
+            <label class="form_item no_border">
+                <input v-model="user" placeholder="usernames" />
+                <!-- <span>&nbsp</span> -->
+                <span>账号有误</span>
+            </label>
+            <label class="form_item no_border">
+                <input v-model="password" placeholder="password" />
+                <!-- <span>&nbsp</span> -->
+                <span>密码有误</span>
+            </label>
+            <!-- 注册part -->
+            <label class="form_item no_border" v-show="!isLogin">
+                <input v-model="againPassword" placeholder="again password" />
+                <!-- <span>&nbsp</span> -->
+                <span>密码不一致</span>
+            </label>
+
+            <div class="form_box_button">
+                <label class="from_item_button">
+                    <!-- 这个按钮就跳转的页面 -->
+                    <button class="form_btn" @click="switchForm">{{ isLogin ? '注册' : '登录' }}</button>
+                </label>
+                <label class="from_item_button">
+                    <!-- 这个按钮是处理逻辑的 -->
+                    <button class="form_btn">{{ isLogin ? '登录' : '注册' }}</button>
+                </label>
+            </div>
+        </div>
+    </main>
+</template>
+<script setup>
+import Nav from '@/components/Nav.vue'
+import { ref } from 'vue'
+
+//data
+const user = ref();
+const password = ref();
+const againPassword = ref();
+
+//是否为登录页面
+const isLogin = ref(true);
+
+function switchForm() {
+    isLogin.value = !isLogin.value;
+}
+
+
+
+</script>
+<style scoped>
+@import url(../../assets/css/view/after/form.css);
+</style>
