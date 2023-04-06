c169e68d602348bb1158edfd47cc74faf5d41ae4
Drunkcat7 <Miaopasc@gmail.com>
2023-04-06 20:22:01 +0800

    update 个人页布局

--- src/views/after/Home.vue
+++ src/views/after/Home.vue
@@ -1,5 +1,35 @@
 <template>
     <Nav />
+    <main>
+        <section class="my_info">
+            <p class="name">Febird</p>
+            <p class="tel">userID : 13410086110</p>
+        </section>
+        <section class="goods_state">
+            <ul>
+                <li>
+                    <van-icon name="coupon" />
+                    <span>待付款</span>
+                </li>
+                <li>
+                    <van-icon name="send-gift" />
+                    <span>待发货</span>
+                </li>
+                <li>
+                    <van-icon name="cart" />
+                    <span>待收货</span>
+                </li>
+                <li>
+                    <van-icon name="comment" />
+                    <span>待评价</span>
+                </li>
+            </ul>
+        </section>
+        <section class="address">
+            <span>地址管理</span>
+            <van-icon name="arrow" />
+        </section>
+    </main>
     
 </template>
 <script setup>
