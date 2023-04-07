07f71c623a3a04a38a093ff07cd63106cb25d797
Jet <1298874688@qq.com>
2023-04-07 09:00:35 +0800

    update 我的订单页面

---
+++ src/components/OrderItem.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="order_item">
+        <van-card num="2" price="2.00" desc="描述信息" title="商品标题"
+            thumb="https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg" />
+        <p style="text-align: right; line-height: 3rem;margin-right: .5rem;">
+            <span style="color:#999;">含运费险服务</span>
+            需付款￥6.40
+        </p>
+        <!-- 不同的状态对应不同的按钮 -->
+        <!-- 
+            0：待付款
+            1：代发货
+            2：待收货
+            3：待评价
+         -->
+        <div v-show="state == 0" class="bottom_column">
+            <van-button class="van_button" type="default" round size="small">修改地址</van-button>
+            <van-button class="van_button" type="default" round size="small">找朋友付</van-button>
+            <van-button class="van_button" color="#FA5E1F" round plain size="small">继续付款</van-button>
+        </div>
+        <div v-show="state == 1 || state == 2" class="bottom_column">
+            <van-button class="van_button" type="default" round size="small">延长收货</van-button>
+            <van-button class="van_button" type="default" round size="small">查看物流</van-button>
+            <van-button class="van_button" color="#FA5E1F" round plain size="small">确认收货</van-button>
+        </div>
+        <div v-show="state == 3" class="bottom_column">
+            <van-button class="van_button" type="default" round size="small">加入购物车</van-button>
+            <van-button class="van_button" type="default" round size="small">查看物流</van-button>
+            <van-button class="van_button" color="#FA5E1F" round plain size="small">评价</van-button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue';
+const props = defineProps(['state'])
+</script>
+<style scoped>
+.order_item {
+    width: 100%;
+    height: fit-content;
+    background-color: white;
+    border-radius: .8rem;
+    margin-bottom: .5rem;
+}
+
+.van_button {
+    padding: 0 1rem;
+    margin-right: .5rem;
+    margin-bottom: .5rem;
+}
+
+.bottom_column {
+    display: flex;
+    justify-content: end;
+}
+</style>
--- src/views/after/MyOrder.vue
+++ src/views/after/MyOrder.vue
@@ -1,10 +1,37 @@
 <template>
-    <Nav />
-    <!-- 订单状态 -->
+    <nav>
+        <van-icon class="back" name="arrow-left" />
+        <p class="title">我的订单</p>
+    </nav>
+    <van-tabs v-model:active="active" type="card" class="box">
+        <van-tab title="全部">
+            <OrderItem :state="0" />
+            <OrderItem :state="1" />
+            <OrderItem :state="2" />
+            <OrderItem :state="3" />
+        </van-tab>
+        <van-tab title="待付款"><OrderItem :state="0" /></van-tab>
+        <van-tab title="待发货"><OrderItem :state="1" /></van-tab>
+        <van-tab title="待收货"><OrderItem :state="2" /></van-tab>
+        <van-tab title="待评价"><OrderItem :state="3" /></van-tab>
+    </van-tabs>
 </template>
 
 <script setup>
 import Nav from '@/components/Nav.vue'
+import OrderItem from '@/components/OrderItem.vue'
+
 </script>
 
 <style scoped src="@/assets/css/view/after/myOrder.css"></style>
+<style>
+.box .van-tabs__nav--card {
+    border-radius: 0.6rem !important;
+    overflow: hidden;
+}
+
+.box .van-tabs__content {
+    min-height: calc(100vh - 4.25rem);
+    padding: 1rem 1rem 0 1rem;
+}
+</style>
