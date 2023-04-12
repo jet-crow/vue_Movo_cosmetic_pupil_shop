68b151b63da241471c8885742ba1d388f8ac4000
Drunkcat7 <Miaopasc@gmail.com>
2023-04-12 11:23:20 +0800

    update OrderManagement back btn

--- src/views/after/OrderManagement.vue
+++ src/views/after/OrderManagement.vue
@@ -1,4 +1,9 @@
 <template>
+    <el-page-header @back="goBack">
+        <template #content>
+            <span class="text-large font-600 mr-3"> 商品管理 </span>
+        </template>
+    </el-page-header>
     <main>
         <ul>
             <li>
