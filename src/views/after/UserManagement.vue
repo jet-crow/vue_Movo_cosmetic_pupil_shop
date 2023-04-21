2cffc2f6efa29c3718063c4d23d6a49a54c57bf3
Drunkcat7 <Miaopasc@gmail.com>
2023-04-21 19:10:55 +0800

    update UserManageme页

--- src/views/after/UserManagement.vue
+++ src/views/after/UserManagement.vue
@@ -1,4 +1,34 @@
 <template>
+    <el-dialog :model-value="addUser" @close="close" title="新增用户信息" width="60%" align-center>
+        <el-input size="large" placeholder="请输入用户名">
+            <template #prepend>账号:</template>
+        </el-input>
+        <div class="h" style="height: 20px;"></div>
+        <el-input size="large" placeholder="请输入密码">
+            <template #prepend>密码:</template>
+        </el-input>
+        <template #footer>
+                <el-button type="primary">
+                    新增
+                </el-button>
+        </template>
+    </el-dialog>
+
+    <el-dialog :model-value="updateUser" @close="close" title="修改用户信息" width="60%" align-center>
+        <el-input size="large" placeholder="请输入用户名" value="{{ addUserData.password }}">
+            <template #prepend>账号:</template>
+        </el-input>
+        <div class="h" style="height: 20px;"></div>
+        <el-input size="large" placeholder="请输入密码">
+            <template #prepend>密码:</template>
+        </el-input>
+        <template #footer>
+                <el-button type="primary">
+                    修改
+                </el-button>
+        </template>
+    </el-dialog>
+
     <el-page-header @back="goBack">
         <template #content>
             <span class="text-large font-600 mr-3"> 商品管理 </span>
@@ -9,7 +39,7 @@
             <el-table-column label="uid" prop="uid" align="center" />
             <el-table-column label="user" prop="user" align="center" />
             <el-table-column label="password" prop="password" align="center" />
-            <el-table-column label="role" prop="level" align="center" >
+            <el-table-column label="role" prop="level" align="center">
                 <template #default="scope">
                     <!-- {{ scope.row.level  }} -->
                     用户
@@ -19,7 +49,7 @@
             <el-table-column align="right" width="200">
                 <template #header>
                     <div class="function_header">
-                        <el-button type="warning" size="small" plain class="add_item" >添加用户</el-button>
+                        <el-button type="warning" size="small" plain class="add_item" @click="addUser = true">添加用户</el-button>
                         <el-input v-model="search" size="small" placeholder="Type to search" />
                     </div>
                 </template>
@@ -42,6 +72,13 @@
 <script setup>
 import { computed, ref, getCurrentInstance } from 'vue';
 
+const addUser = ref(false);
+const updateUser = ref(false);
+const addUserData = ref({
+    username:null,
+    password:11,
+})
+
 const { proxy } = getCurrentInstance();
 let accountData = ref([]);
 
@@ -60,6 +97,7 @@ const filterAccountData = computed(() => {
 });
 const handleEdit = (index, row) => {
     console.log(index, row);
+    updateUser.value = true;
 };
 const handleDelete = (index, row) => {
     console.log(index, row);
